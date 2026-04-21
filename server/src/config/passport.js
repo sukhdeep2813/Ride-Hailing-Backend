d3c1bad67389703d5b0b3bb88f54e356f6deeb9b
sukhdeep <sukhdeepkumar918@gmail.com>
2026-04-21 17:50:44 +0530

    Updated Passport Logic

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -4,29 +4,39 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: clientID.process.env.VITE_GOOGLE_CLIENT_ID,
+      clientSecret: clientSecret.process.env.VITE_GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const user = await User.findOne({ googleId: profile.id });
 
-passport.use(new GoogleStrategy({
-    clientID : clientID.process.env.VITE_GOOGLE_CLIENT_ID,
-    clientSecret : clientSecret.process.env.VITE_GOOGLE_CLIENT_SECRET,
-    callbackURL : "/auth/google/callback",
-},
-async(accessToken, refreshToken, Profiler, donne) =>{
-    try {
-        const user = await User.findOne({googleId : profile.id});
-
-        if(!user){
-            const newUser = await User.create({
-                googleId : profile.id,
-                displayName : profile.displayName,
-                email : profile.emails[0].value,
-            });
+        if (!user) {
+          const newUser = await User.create({
+            googleId: profile.id,
+            displayName: profile.displayName,
+            email: profile.emails[0].value,
+            avatar: profile.photos[0].value,
+          });
         }
 
         return done(null, user);
-    } catch (error) {
+      } catch (error) {
         return done(error, null);
-    }
-}
-));
+      }
+    },
+  ),
+);
 
 //serilize and deserialize user
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
+export default passport;
