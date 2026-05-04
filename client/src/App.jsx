a203ca1229d52359b90f2d6511dfc25bd78aff19
sukhdeep <sukhdeepkumar918@gmail.com>
2026-05-04 18:22:27 +0530

    After Login DashBoard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Landing from "./pages/Landing";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import DashBoard from "./pages/DashBoard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<DashBoard />} />
     </Routes>
   );
 }
---
+++ client/src/pages/DashBoard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashBoard = () => {
+  return <div>Welcome Back</div>;
+};
+
+export default DashBoard;
