258a555a911139828c652e9ad2bdd4a04d35962f
sukhdeep <sukhdeepkumar918@gmail.com>
2026-06-03 21:58:56 +0530

    Removing Debugging Logs

--- client/src/pages/MapDashboard.jsx
+++ client/src/pages/MapDashboard.jsx
@@ -3,13 +3,6 @@ import MapContainer from "../components/MapContainer";
 import { APIProvider } from "@vis.gl/react-google-maps";
 
 const MapDashboard = () => {
-  console.log(
-    "Loaded API Key Status:",
-    !!import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-  );
-  console.log("What Vite actually sees:", import.meta.env);
-  console.log(import.meta.env);
-
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <div className="w-full h-full relative">
