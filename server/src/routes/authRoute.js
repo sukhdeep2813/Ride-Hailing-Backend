1898e5fbe3002fdb925df261e56009dd98e5df24
sukhdeep <sukhdeepkumar918@gmail.com>
2026-06-11 20:53:46 +0530

    Post route for signup

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { googleAuth, googleAuthCallback } from "../controllers/authController.js";
+import { googleAuth, googleAuthCallback, SignUpController } from "../controllers/authController.js";
 
 const router = express.Router();
 
 //Routes 
 router.get('/google', googleAuth);
 router.get('/google/callback', googleAuthCallback);
+
+
+router.post('/signup', SignUpController); // Implement this controller to handle user registration
+// router.post('/login', LoginController); // Implement this controller to handle user login
 
 export default router;
