395ec5cf1eb307f2aec1a8acac48f36cdf917b84
sukhdeep <sukhdeepkumar918@gmail.com>
2026-06-20 14:07:41 +0530

    Update

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -5,6 +5,7 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 const fetchWithAuth = async (endpoint, options) => {
   //  get token  form localStorage
   const token = localStorage.getItem("token");
+
   //add headers
   const headers = {
     "Content-Type": "application/json",
