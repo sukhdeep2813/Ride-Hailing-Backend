99074c57641e5eee1f1535bcc74b4c5fb6bf0357
sukhdeep <sukhdeepkumar918@gmail.com>
2026-06-27 15:36:24 +0530

    Money in Integer + DifferentTimes

--- server/src/controllers/fareController.js
+++ server/src/controllers/fareController.js
@@ -10,21 +10,60 @@ export const calculateFare = async (req, res) => {
     }
 
     const vehicleRates = {
-      BoltBike: { name: "Bolt Bike", base: 25, perKm: 8, perMin: 1.5 },
-      BoltMini: { name: "Bolt Mini", base: 40, perKm: 13, perMin: 2.0 },
-      BoltSedan: { name: "Bolt Sedan", base: 55, perKm: 16, perMin: 2.5 },
-      BoltSuite: { name: "Bolt Suite", base: 80, perKm: 22, perMin: 3.5 },
-      BoltMinini: { name: "Bolt Go", base: 45, perKm: 14, perMin: 2.0 },
-      BoltSedan2: { name: "Bolt Plus", base: 60, perKm: 18, perMin: 2.5 },
+      BoltBike: {
+        name: "Bolt Bike",
+        base: 25,
+        perKm: 8,
+        perMin: 1.5,
+        speedMultiplier: 0.75,
+      },
+      BoltMini: {
+        name: "Bolt Mini",
+        base: 40,
+        perKm: 13,
+        perMin: 2.0,
+        speedMultiplier: 1.0,
+      },
+      BoltSedan: {
+        name: "Bolt Sedan",
+        base: 55,
+        perKm: 16,
+        perMin: 2.5,
+        speedMultiplier: 1.0,
+      },
+      BoltSuite: {
+        name: "Bolt Suite",
+        base: 80,
+        perKm: 22,
+        perMin: 3.5,
+        speedMultiplier: 0.95,
+      },
+      BoltMinini: {
+        name: "Bolt Go",
+        base: 45,
+        perKm: 14,
+        perMin: 2.0,
+        speedMultiplier: 0.95,
+      },
+      BoltSedan2: {
+        name: "Bolt Plus",
+        base: 60,
+        perKm: 18,
+        perMin: 2.5,
+        speedMultiplier: 0.9,
+      },
     };
 
     const calculateOption = Object.keys(vehicleRates).map((id) => {
       const rate = vehicleRates[id];
 
+      const dynamicDuration = Math.round(durationMin * rate.speedMultiplier);
       const fareAmount =
         rate.base + distanceKm * rate.perKm + durationMin * rate.perMin;
 
-      return { id, name: rate.name, fareAmount, time: `${durationMin} min` };
+      const fare = Math.round(fareAmount);
+
+      return { id, name: rate.name, fare, time: `${dynamicDuration} min` };
     });
     return res.status(200).json({
       success: true,
