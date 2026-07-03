d7e29a5ef28008038e3f671a697c9d7d9785c8ba
sukhdeep <sukhdeepkumar918@gmail.com>
2026-07-03 16:47:18 +0530

    Two interface

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,29 @@ import Setting from "./pages/Setting";
 import History from "./pages/History";
 import Payments from "./pages/Payments";
 import Profile from "./pages/Profile";
-import { LayoutProvider } from "./context/LayoutContext";
+import { LayoutProvider, useLayout } from "./context/LayoutContext";
 import { Toaster } from "react-hot-toast";
 import SignUp from "./pages/SignUp";
+import DriverDashboard from "./components/DriverDashboard";
+
+const DashBoardWrapper = () => {
+  const { profile } = useLayout(); // Access the profile from LayoutContext
+
+  // If the profile data is still being loaded by your useEffect hook
+  if (!profile) {
+    return (
+      <div className="min-h-screen bg-black text-white flex items-center justify-center">
+        <div className="w-6 h-6 border-2 border-t-orange-500 border-zinc-800 rounded-full animate-spin" />
+      </div>
+    );
+  }
+
+  if (profile.role === "DRIVER") {
+    return <DriverDashboard />;
+  }
+
+  return <MapDashboard />;
+};
 
 function App() {
   return (
@@ -34,7 +54,7 @@ function App() {
           }
         >
           {/* 2. When on exactly /dashboard, load the Map & Widget */}
-          <Route index element={<MapDashboard />} />
+          <Route index element={<DashBoardWrapper />} />
 
           {/* 3. Sub-routes render inside DashBoard's <Outlet /> */}
           <Route path="settings" element={<Setting />} />
