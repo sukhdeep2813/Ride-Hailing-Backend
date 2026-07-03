3f447057c2147bc2c350fdb55517a804fe5f0d26
sukhdeep <sukhdeepkumar918@gmail.com>
2026-07-03 16:49:57 +0530

    Route for pending

--- server/src/routes/rideRoute.js
+++ server/src/routes/rideRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectedRoute } from "../middlewares/authMiddleware.js";
 import { getRiderHistory } from "../controllers/rideHistoryController.js";
-import { createRide } from "../controllers/rideController.js";
+import { createRide, getPendingRides } from "../controllers/rideController.js";
 
 const router = express.Router();
 
 router.get("/history", protectedRoute, getRiderHistory);
 router.post("/create", protectedRoute, createRide);
+router.get("/pending", protectedRoute, getPendingRides);
 
 export default router;
