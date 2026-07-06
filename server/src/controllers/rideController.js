40761a9bf891e283a338b0af03996422ea7c6a98
sukhdeep <sukhdeepkumar918@gmail.com>
2026-07-06 13:51:37 +0530

    Accepting Pending Rides

--- server/src/controllers/rideController.js
+++ server/src/controllers/rideController.js
@@ -50,16 +50,61 @@ export const createRide = async (req, res) => {
 
 export const getPendingRides = async (req, res) => {
   try {
+    const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000); // 30 minutes ago
     const rides = await prisma.ride.findMany({
-      where: { status: "PENDING" },
+      where: { status: "PENDING", createdAt: { gte: thirtyMinutesAgo } },
       orderBy: { createdAt: "desc" },
     });
-    console.log("Fetched pending rides:", rides);
+
     return res.status(200).json({ sucess: true, rides });
   } catch (error) {
     console.error("Error fetching pending rides, rides", error);
     return res
       .status(500)
       .json({ sucess: false, message: "Internal server error" });
+  }
+};
+
+export const acceptRideJob = async (req, res) => {
+  try {
+    const { rideId } = req.params;
+    const driverId = req.user.id;
+
+    const existingRide = await prisma.ride.findUnique({
+      where: { id: rideId },
+    });
+
+    if (!existingRide) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Ride not found" });
+    }
+    if (existingRide.status !== "PENDING") {
+      return res.status(400).json({
+        success: false,
+        message: "This ride has already been taken by another driver.",
+      });
+    }
+
+    //update now in database
+    const updateRides = await prisma.ride.update({
+      where: { id: rideId },
+      data: {
+        status: "ACCEPTED",
+        driverId: driverId,
+      },
+      include: { rider: { select: { name: true } } },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Ride Accepted successfully",
+      ride: updateRides,
+    });
+  } catch (error) {
+    console.error("Error accepting ride job:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Server error accepting ride job." });
   }
 };
