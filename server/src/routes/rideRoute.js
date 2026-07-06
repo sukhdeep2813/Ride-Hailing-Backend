d431f280aded3fcbd7683c10b461fd9ba7a22c47
sukhdeep <sukhdeepkumar918@gmail.com>
2026-07-06 13:51:54 +0530

    New route for Patch

--- server/src/routes/rideRoute.js
+++ server/src/routes/rideRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { protectedRoute } from "../middlewares/authMiddleware.js";
 import { getRiderHistory } from "../controllers/rideHistoryController.js";
-import { createRide, getPendingRides } from "../controllers/rideController.js";
+import { acceptRideJob, createRide, getPendingRides } from "../controllers/rideController.js";
 
 const router = express.Router();
 
 router.get("/history", protectedRoute, getRiderHistory);
 router.post("/create", protectedRoute, createRide);
 router.get("/pending", protectedRoute, getPendingRides);
+router.patch("/accept/:rideId", protectedRoute, acceptRideJob);
 
 export default router;
