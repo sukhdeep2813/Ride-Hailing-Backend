38e1781d0b95be1259aa4f9e87c6e9aa97158b0e
sukhdeep <sukhdeepkumar918@gmail.com>
2026-07-18 14:07:48 +0530

    route for nearbyDrivers

--- server/src/routes/driverRoutes.js
+++ server/src/routes/driverRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
 import { driverStore } from "../store/driverStore.js";
+import { protectedRoute } from "../middlewares/authMiddleware.js";
+import { getNearbyDrivers } from "../controllers/nearbyController.js";
 
 const router = express.Router();
 
+router.get("/nearby", protectedRoute, getNearbyDrivers);
+
+export default router;
