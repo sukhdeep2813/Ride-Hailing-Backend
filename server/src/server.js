6a7153183f874798587c0d2790c6d4bb80cd7cdb
sukhdeep <sukhdeepkumar918@gmail.com>
2026-07-26 16:30:51 +0530

    Endpoint settle

--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,7 @@ import authRoute from "./routes/authRoute.js";
 import fareRoute from "./routes/fareRoute.js";
 import rideRoute from "./routes/rideRoute.js";
 import { initDriverSockets } from "../sockets/driverSocket.js"; // 3. Import your socket handler
-// import driverRoutes from "./routes/driverRoutes.js";
+import driverRoutes from "./routes/driverRoutes.js";
 
 dotenv.config();
 
@@ -56,7 +56,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/fare", fareRoute);
 app.use("/api/rides", rideRoute);
 app.use("/api/create", rideRoute);
-// app.use("/api/drivers", driverRoutes);
+app.use("/api/drivers", driverRoutes);
 
 
 app.get("/", (req, res) => {
